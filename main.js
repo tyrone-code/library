563fd22e5fb5186bbbece735d520b6ec5f0d8eab
Tyrone <tschoeman7@gmail.com>
2024-08-23 17:57:40 +0200

    added code that allows new objects using the new keyword created from the constructor function to be pushed onto an array

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
+const bookArrObjects = [];
+
 const container = document.querySelector(".container");
 
-const bookArrObjects = [
-  {
-    author: "George Orwell",
-    pages: 803,
-    title: "1984",
-  },
-  { author: "Various", pages: 1200, title: "Bible" },
-];
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
+function addBookToLibrary(title, author, pages) {
+  let p = new Book(title, author, pages); // here we create instance
+  bookArrObjects.push(p);
+}
+// Creating objects using the constructor function
+
+addBookToLibrary("Alice in wonderland", "Lewis carroll", 352);
+addBookToLibrary("1984", "George Orwell", 328);
+addBookToLibrary("Art of war", "Sun tzu", 260);
 
 bookArrObjects.forEach((item) => {
   const box = document.createElement("div");
