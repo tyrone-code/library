88248ba940a0621a258a0a8b707812f7a8e9315f
Tyrone <tschoeman7@gmail.com>
2024-08-23 17:17:30 +0200

    added code that DOM displays on page from array of objects, added css to make library cards look nicer

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log("hello world!");
+const container = document.querySelector(".container");
+
+const bookArrObjects = [
+  {
+    author: "George Orwell",
+    pages: 803,
+    title: "1984",
+  },
+  { author: "Various", pages: 1200, title: "Bible" },
+];
+
+bookArrObjects.forEach((item) => {
+  const box = document.createElement("div");
+  box.className = "card";
+
+  const list = document.createElement("p");
+
+  list.innerText = `
+   Title: ${item.title}
+  Author:  ${item.author}
+   Pages: ${item.pages}
+  `;
+  box.appendChild(list);
+
+  container.appendChild(box);
+});
