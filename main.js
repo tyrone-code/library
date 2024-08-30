7bc27a128e89046e2c2874927ad8163c0882a999
Tyrone <tschoeman7@gmail.com>
2024-08-30 11:02:47 +0200

    added toggle function to read status

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
-// Array to hold book objects
-let bookArrObjects = [];
-
-// Get references to DOM elements
-let container = document.querySelector(".container");
-let addNewBook = document.getElementById("new-book");
-let deleteCard = null;
-let confirm = document.getElementById("confirm");
-let checkBox = document.getElementById("readBook?")
-
-
-
-const cancelButton = document.getElementById("cancel");
-const dialog = document.getElementById("book-details");
-dialog.returnValue = "book-details";
-
-
-
 // Constructor function to create book objects
 function Book(title, author, pages) {
   this.title = title;
@@ -23,11 +5,23 @@ function Book(title, author, pages) {
   this.pages = pages;
 }
 
+// Array to hold book objects
+let bookArrObjects = [];
+
+// Get references to DOM elements
+let container = document.querySelector(".container");
+let addNewBook = document.getElementById("new-book");
+let confirm = document.getElementById("confirm");
+let cancelButton = document.getElementById("cancel");
+let checkBox = document.getElementById("readBook?");
+let dialog = document.getElementById("book-details");
+dialog.returnValue = "book-details";
+
 // Function to get user input and call the callback function
 let getUserInput = function (callback) {
-  let titleBook = document.getElementById("title").value
-  let authorBook = document.getElementById("author").value
-  let pagesBook = document.getElementById("pages").value
+  let titleBook = document.getElementById("title").value;
+  let authorBook = document.getElementById("author").value;
+  let pagesBook = document.getElementById("pages").value;
   callback(titleBook, authorBook, pagesBook);
 };
 
@@ -37,105 +31,78 @@ function addBookToLibrary(title, author, pages) {
   bookArrObjects.push(p);
 }
 
-
+// Function to display the modal dialog
 function displayModal() {
   dialog.showModal();
-
-  
-
 }
 
-function displayCard(){
-    getUserInput(addBookToLibrary);
+// Function to display a card for each book in the library
+function displayCard() {
+  getUserInput(addBookToLibrary);
+
   bookArrObjects.forEach((item) => {
     const box = document.createElement("div");
     const yes = document.createElement("img");
-    yes.id = "yes"
+    yes.id = "yes";
     yes.setAttribute("src", "/images/yes.svg");
-    const no = document.createElement("img");
-    no.id = "no"
+    let no = document.createElement("img");
+    no.id = "no";
     no.setAttribute("src", "/images/no.svg");
- 
- 
     const img = document.createElement("img");
     box.className = "card";
     img.classList.add("delete", "bounce");
     img.setAttribute("src", "/images/delete.svg");
     const list = document.createElement("p");
     const read = document.createElement("p");
-    read.innerText = "Read Status"
-     read.className = "read"
+    read.innerText = "Read Status";
+    read.className = "read";
     list.innerText = `
      Title - ${item.title}
-     Author -  ${item.author}
+     Author - ${item.author}
      Pages - ${item.pages}
     `;
     box.appendChild(list);
     box.appendChild(read);
-    box.appendChild(img)
-    // box.appendChild(yes)
-    if (checkBox.checked === true){
-      box.appendChild(yes)
-      yes.style.display = "block"
-    }else {
-      box.appendChild(no)
-      no.style.display = "block"
-
+    box.appendChild(img);
+    
+    if (checkBox.checked) {
+      box.appendChild(yes);
+      yes.style.display = "block";
+    } else {
+      box.appendChild(no);
+      no.style.display = "block";
     }
 
     container.appendChild(box);
     deleteCard = document.querySelectorAll(".delete");
     bookArrObjects = [];
+
+    yes.addEventListener("click", function() {
+      box.appendChild(no);
+      yes.style.display = "none";
+      no.style.display = "block";
+    });
+
+    no.addEventListener("click", function() {
+      box.appendChild(yes);
+      yes.style.display = "block";
+      no.style.display = "none";
+    });
   });
 
-  
-
   deleteCard.forEach(item => {
-    console.log(item)
     item.addEventListener('click', function() {
-      let redTrashCanParent = this.parentNode // which will  be card
-      redTrashCanParent.remove()
-      
-      })
-    })
-    dialog.close("book-details")
+      let redTrashCanParent = this.parentNode; // which will be card
+      redTrashCanParent.remove();
+    });
+  });
+  
+  dialog.close("book-details");
 }
-
 
 // Event listeners for button clicks
 addNewBook.addEventListener("click", displayModal);
-confirm.addEventListener("click", displayCard)
-
-// ------------------------------------------------------------------------//
-
-
-// Form cancel button closes the dialog box
+confirm.addEventListener("click", displayCard);
 cancelButton.addEventListener("click", () => {
   dialog.close("book-details");
 });
-
-
-
-
-// function change() {
-//   var img1 = "http://placehold.it/350x150",
-//       img2 = "http://placehold.it/200x200";
-//   var imgElement = document.getElementById('test');
-  
-//   imgElement.src = (imgElement.src === img1)? img2 : img1;
-// }
-
-// function Person(first, last, age, eye) {
-//   this.firstName = first;
-//   this.lastName = last;
-//   this.age = age;
-//   this.eyeColor = eye;
-// }
-
-// Book.prototype.change = function() {
-//   return this.firstName + " " + this.lastName
-// };
-
-// const myFather = new Person("John", "Doe", 50, "blue");
-// document.getElementById("demo").innerHTML =
-// "My father is " + myFather.name(); 
